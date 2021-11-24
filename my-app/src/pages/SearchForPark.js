53c90e25f2dcae651edc1e3e1536c7b82d2d5656
CaHerm <cam2000h@gmail.com>
2021-11-24 16:20:01 -0700

    Added basic search bar

--- my-app/src/pages/SearchForPark.js
+++ my-app/src/pages/SearchForPark.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const SearchForParkPage = () =>{
 
-    return <div>Search for a park page</div>
+	return   <form>
+	           <h2>
+	    		  Search for a park page:
+	    	   </h2>
+	    	   <div>
+	    	     <label>Park name: </label>
+	              <input
+	                type='text'
+	                placeholder='park name'
+	              ></input>
+	              <button>
+	                Search
+	              </button>
+	            </div>
+	            
+	          </form>
 
 };
 
