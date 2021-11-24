8c3c1924b912f273f89a0ca6be17c98b2ce86201
MuizMuhammad99 <muiz.muhammad99@gmail.com>
2021-11-24 13:54:32 -0700

    Forms for Register & Login Page

--- my-app/src/pages/LogIn.js
+++ my-app/src/pages/LogIn.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 const LogIn = () =>{
 
-    return <div>Login page</div>
+    return <div>
+        <form>
+                        <div>
+                            <label>Username</label>
+                            <input
+                                type='text'
+                                placeholder='Username'
+                                ></input>
+                        </div>
+                        <div >
+                            <label>Password</label>
+                            <input
+                                type='text'
+                                placeholder='Password'
+                                ></input>
+                        </div>
+                        <div>
+                            <button>
+                                Log In
+                            </button>
+                        </div>
+                    </form>
+    </div>
 
 };
 
--- my-app/src/pages/Register.js
+++ my-app/src/pages/Register.js
@@ -2,7 +2,36 @@ import React from 'react';
 
 const Register = () =>{
 
-    return <div>Register page</div>
+    return <div>
+        <form>
+                        <div>
+                            <label>Username</label>
+                            <input
+                                type='text'
+                                placeholder='Username'
+                                ></input>
+                        </div>
+                        <div >
+                            <label>Email</label>
+                            <input
+                                type='text'
+                                placeholder='Email'
+                                ></input>
+                        </div>
+                        <div >
+                            <label>Password</label>
+                            <input
+                                type='text'
+                                placeholder='Password'
+                                ></input>
+                        </div>
+                        <div>
+                            <button>
+                                Sign Up
+                            </button>
+                        </div>
+                    </form>
+    </div>
 
 };
 
