20604f236824e066d6a0cc44177c0099d1d60a2c
CaHerm <cam2000h@gmail.com>
2021-11-24 18:54:25 -0700

    Added a background

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <div className = "Primary">
       <Router>
         <NavigationBar />
         <Routes />
