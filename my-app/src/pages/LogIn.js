4870c008b7a70848c7166041a34ba6e281d25401
MuizMuhammad99 <muiz.muhammad99@gmail.com>
2021-11-25 16:48:38 -0700

    finalized styling of homepage, register page, login page and buttons + forms

--- my-app/src/pages/LogIn.js
+++ my-app/src/pages/LogIn.js
@@ -1,27 +1,30 @@
 import React from 'react';
+import '../style/login.css';
 
 
 const LogIn = () =>{
 
-    return <div>
-        <h1 className = "Title">User Login</h1>
-        <form>
-                        <div>
-                            <label>Username</label>
-                            <input
+    return <div className ="loginImage">
+        <form className ="secondlogin">
+        <h1 style ={{textAlign: 'center'}}>User Login</h1>
+                        <div style ={{textAlign: 'center'}}>
+                            <label className = "labelstyle">Username</label>
+                            <input 
+                                className = "inputstyle"
                                 type='text'
                                 placeholder='Username'
                                 ></input>
                         </div>
-                        <div >
-                            <label>Password</label>
+                        <div style ={{textAlign: 'center'}}>
+                            <label className = "labelstyle">Password</label>
                             <input
+                                className = "inputstyle"
                                 type='text'
                                 placeholder='Password'
                                 ></input>
                         </div>
-                        <div>
-                            <button>
+                        <div style ={{textAlign: 'center', marginTop: "50px", marginBottom: "50px"}}>
+                            <button className= "btn">
                                 Log In
                             </button>
                         </div>
