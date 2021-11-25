99f36ba0d88ab7c8050d82a2a2def73491f40b51
CaHerm <cam2000h@gmail.com>
2021-11-24 19:03:29 -0700

    Added table of parks w/ test values

--- my-app/src/pages/SeeAllParks.js
+++ my-app/src/pages/SeeAllParks.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React , {useState} from 'react';
+
+function Park(props) {
+	return(
+		<div className = "Menu-bar">
+			<h4 className = "Table-element" >{props.name}</h4>
+			<h4>{props.quadrant}</h4>
+		</div>
+	);
+}
+
 
 const SeeAllParks = () =>{
 
-    return <div>Here are all the parks </div>
+	const [park, setParks] = useState([
+		{name: "Test Park 1", quadrant: "NE"},
+		{name: "Test Chaparral Park", quadrant: "SE"},
+		{name: "Test Stampede Park", quadrant: "Central"}
+	]);
+
+    return <div>
+    			<h1 className = "Title"> Calgary dog parks </h1>
+    			<div className = "Table-title">
+    				<h2>Park Name</h2>
+    				<h2>Quadrant</h2>
+    			</div>
+    			{park.map(park => (
+    				<Park name={park.name} quadrant={park.quadrant} />
+    			))}
+    	   </div>
 
 };
 
