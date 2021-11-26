ed9086cd048a702d6070e3f6c8b8f07ff474cf2b
MuizMuhammad99 <muiz.muhammad99@gmail.com>
2021-11-25 21:45:24 -0700

    about page

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -3,9 +3,11 @@ import React from 'react';
 
 const AboutPage = () =>{
 
-    return <div>
-    		<h1 className = "Title">About Us</h1>
-    		This website was designed by four students from the University of Calgary to provide a convenient way for Calgarians to browse their city's extensive selection of dog parks.
+    return <div className = "aboutImage">
+    		<div className ="aboutsecond" style ={{textAlign: 'center'}}>
+			<h1 className = "Title">About Us</h1>
+    		<h3>This website was designed by four students from the University of Calgary to provide a convenient way for Calgarians to browse their city's extensive selection of dog parks.</h3>
+			</div>
     	   </div>
 
 };
