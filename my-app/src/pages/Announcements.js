13041d68d36f7f7e0a6bb62994f99fa5ede6f86f
CaHerm <cam2000h@gmail.com>
2021-12-17 12:03:34 -0700

    last contribution, fixed up announcements page

--- my-app/src/pages/Announcements.js
+++ my-app/src/pages/Announcements.js
@@ -1,35 +1,41 @@
 import React, {useState} from 'react';
 import '../App.css'
 
+
+
 function Event(props) {
-	return(
-		<div className = "Menu-bar" >
-			<h4 className = "Table-element" >{props.name}</h4>
-			<h4>{props.heldBy}</h4>
-		</div>
-	);
+  return(
+    <div className="Park-list">
+      <div className="Menu-bar">
+        <h3 className="Table-element">{props.name}</h3>
+        <h3 className="Table-element">{props.date}</h3>
+      </div>
+      <h4>Description: {props.desc}</h4>
+    </div>
+  );
 }
 
 const AnnouncementsPage = () =>{
 
-	const [event, setEvents] = useState([
-		{name: "Test event 1", heldBy: "We Love Dogs Foundation"},
-		{name: "Berners and Buds", heldBy: "B&B Group"},
-		{name: "Exciting dog park event", heldBy: "City of Calgary"}
-	]);
+	  const [evs, setEvs] = React.useState({ a: [] });
+
+	  React.useEffect(() => {
+	    fetch("/api/event/all")
+	      .then((res) => res.json())
+	      .then((evs) => setEvs({ a: evs }));
+	  }, []);
 
     return  <div className = "announcementImage">
     			<div className = "announcementsecond" style ={{textAlign: 'center'}}>
 				<h1 className = "Title">Active Events</h1>
     			<div className = "Table-title">
 				<div >
-				<h1 >Active Events</h1>
+				
 				</div >
-    				<h2>Event Name</h2>
-    				<h2>Organization</h2>
+    			
     			</div>
-    			{event.map(event => (
-    				<Event name={event.name} heldBy={event.heldBy} />
+    			{evs.a.map(x => (
+    				<Event name={x.Name} date={x.Date} desc={x.Description} />
     			))}
 				</div>
     		</div>
