669ce8e843eac41e935a1f715b3b9e0bd33d0b32
MuizMuhammad99 <muiz.muhammad99@gmail.com>
2021-12-17 00:18:53 -0700

    Update LogIn.js

--- my-app/src/pages/LogIn.js
+++ my-app/src/pages/LogIn.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import '../style/login.css';
-
+import { useState} from "react";
+import Axios from 'axios';
+import styles from "../../css/SignIn.module.css"
 
 const LogIn = () =>{
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = () => {
+        Axios.post("http://localhost:3001/login", {
+          username: username,
+          password: password,
+        }).then((response) => {
+          console.log(response);
+        });
+      };
 
     return <div className ="loginImage">
         <form className ="secondlogin">
@@ -13,6 +27,9 @@ const LogIn = () =>{
                                 className = "inputstyle"
                                 type='text'
                                 placeholder='Username'
+                                onChange={(e) => {
+                                    setUsername(e.target.value);
+                                }}
                                 ></input>
                         </div>
                         <div style ={{textAlign: 'center'}}>
@@ -21,10 +38,13 @@ const LogIn = () =>{
                                 className = "inputstyle"
                                 type='text'
                                 placeholder='Password'
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                }}
                                 ></input>
                         </div>
                         <div style ={{textAlign: 'center', marginTop: "50px", marginBottom: "50px"}}>
-                            <button className= "btn">
+                            <button className= "btn" onClick={login}>
                                 Log In
                             </button>
                         </div>
