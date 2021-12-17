3e571d16a90bee765a5b8a263c7c959497e270f9
CaHerm <cam2000h@gmail.com>
2021-12-17 09:15:48 -0700

    Added search functions to search page

--- my-app/src/pages/SearchForPark.js
+++ my-app/src/pages/SearchForPark.js
@@ -3,6 +3,10 @@ import '../style/register.css'
 
 const SearchForParkPage = () =>{
 
+    const [name, setName] = React.useState("none");
+    const [hood, setHood] = React.useState("none");
+    const [add,  setAdd ] = React.useState("none");
+
 	return   <div className ="searchImage">
         <form className ="searchsecond">
         <h1 style ={{textAlign: 'center'}}>Search for a dog park in Calgary</h1>
@@ -12,11 +16,33 @@ const SearchForParkPage = () =>{
                                 className = "inputstyle"
                                 type='text'
                                 placeholder='Park name'
+                                onChange={(e) => {setName(e.target.value)}}
+                                ></input>
+                        </div>
+                        
+                        
+                        <div style ={{textAlign: 'center'}}>
+                            <label className = "labelstyle">Park neighbourhood</label>
+                            <input 
+                                className = "inputstyle"
+                                type='text'
+                                placeholder='Park neighbourhood'
+                                onChange={(e) => {setHood(e.target.value)}}
+                                ></input>
+                        </div>
+
+                        <div style ={{textAlign: 'center'}}>
+                            <label className = "labelstyle">Park street</label>
+                            <input 
+                                className = "inputstyle"
+                                type='text'
+                                placeholder='Park street'
+                                onChange={(e) => {setAdd(e.target.value)}}
                                 ></input>
                         </div>
                         
                         <div style ={{textAlign: 'center', marginTop: "50px", marginBottom: "50px"}}>
-                            <button className= "btn">
+                            <button className= "btn" onClick={() => {window.open("http://localhost:3000/SeeAllParks?name="+name+"&hood="+hood+"&add="+add)}}>
                                 Search
                             </button>
                         </div>
